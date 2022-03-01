808a9c86802bb429586810864a0483720b074ab4
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2022-03-01 21:50:48 +0100

    Add LoadScene method to FinishLine and CrashDetector class

--- Assets/Script/FinishLine.cs
+++ Assets/Script/FinishLine.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
     private string tagPlayer = "Player";
+    private int levelOne = 0;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == tagPlayer)
         {
-            Debug.Log("You finished");
+            SceneManager.LoadScene(levelOne);
         }
     }
 }
